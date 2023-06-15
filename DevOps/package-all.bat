80339e6441442fb176b3956f61fa97ecfe734005
Denis Maltsev <maltsevda@yandex.ru>
2023-06-15 11:21:34 +0400

    New engine support 5.2

--- DevOps/package-all.bat
+++ DevOps/package-all.bat
@@ -3,3 +3,4 @@
 CALL "%~dp0package-plugin.bat" 4.27 "E:\EpicGames\UE_4.27_Plus"
 CALL "%~dp0package-plugin.bat" 5.0 "E:\EpicGames\UE_5.0"
 CALL "%~dp0package-plugin.bat" 5.1 "E:\EpicGames\UE_5.1"
+CALL "%~dp0package-plugin.bat" 5.2 "E:\EpicGames\UE_5.2"
